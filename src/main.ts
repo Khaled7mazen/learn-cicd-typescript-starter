f39629770e198fad3b4a0fce9c5c0c0b7faa425d
khaled <khaledmazen2002@gmail.com>
2026-03-10 01:50:33 +0200

    test lint failure

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,12 @@ import { handlerUsersCreate, handlerUsersGet } from "./api/users.js";
 
 const __dirname = path.resolve();
 
+
+function unused() {
+  // this function does nothing
+  // and is called nowhere
+} 
+
 if (!config.api.port) {
   console.error("PORT environment variable is not set");
   process.exit(1);
@@ -47,3 +53,5 @@ app.use("/v1", v1Router);
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
+
+
